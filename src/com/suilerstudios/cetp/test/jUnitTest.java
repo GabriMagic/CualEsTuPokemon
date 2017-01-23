98b9ceed862a7a256a6c91476cd5437541b6b9e2
GabriMagic <champy9a@gmail.com>
2017-01-23 12:31:59 +0000

    d

---
+++ src/com/suilerstudios/cetp/test/jUnitTest.java
@@ -0,0 +1,14 @@
+package com.suilerstudios.cetp.test;
+
+import org.junit.Test;
+
+import com.suilerstudios.cetp.modelo.Pokemon;
+
+public class jUnitTest {
+
+	@Test
+	public void test() {
+		Pokemon poke = new Pokemon("Pikachu");
+	}
+
+}
