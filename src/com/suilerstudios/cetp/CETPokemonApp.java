265ad9ad39c45e6662ac58a957bd55717f48c1e4
GabriMagic <champy9a@gmail.com>
2017-01-26 10:11:23 +0000

    asd

--- src/com/suilerstudios/cetp/CETPokemonApp.java
+++ src/com/suilerstudios/cetp/CETPokemonApp.java
@@ -16,7 +16,6 @@ public class CETPokemonApp extends Application{
 		primaryStage.initStyle(StageStyle.TRANSPARENT);
 		MainController main = new MainController(primaryStage);
 		Scene pokemon = new Scene(main.getVentana().getRoot());
-		System.out.println("Inicio");
 		pokemon.setFill(Color.TRANSPARENT);
 		
 		
