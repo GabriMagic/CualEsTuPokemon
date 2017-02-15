6947a6ee466243e72fc8836368bda83dd7fcbc71
Xilerth <Xilerth@gmail.com>
2017-02-15 13:44:11 +0000

    main

---
+++ src/com/suilerstudios/cetp/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.suilerstudios.cetp.controller;
+
+import com.suilerstudios.cetp.modelo.Partida;
+
+public class MainController {
+
+	VentanaPrincipalController ventana;
+	Partida partida;
+	
+	
+}
