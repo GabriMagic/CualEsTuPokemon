35f27ce3150d6c1089f7edd5c56915b8c096be85
GabriMagic <champy9a@gmail.com>
2017-02-23 17:25:12 +0000

    w

--- src/com/suilerstudios/cetp/CETPokemonApp.java
+++ src/com/suilerstudios/cetp/CETPokemonApp.java
@@ -17,31 +17,33 @@ public class CETPokemonApp extends Application {
 	private Scene pokemon;
 
 	private MenuController menu;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		JsonFinder.generarListaPokemon();
-		
+
 		intro = new SoundPlayer(getClass().getResourceAsStream("/com/suilerstudios/cetp/sounds/intro.mp3"));
 		intro.start();
-		
+
 		menu = new MenuController(primaryStage);
 		primaryStage.initStyle(StageStyle.TRANSPARENT);
-		
+
 		pokemon = new Scene(menu.getRoot());
 		pokemon.setFill(Color.TRANSPARENT);
 
 		primaryStage.setTitle("¿Cuál es tu Pokémon?");
 		primaryStage.setResizable(false);
-		
+
 		// carga de icono
 		primaryStage.getIcons().add(new Image("/com/suilerstudios/cetp/img/logo.png"));
 
 		primaryStage.setScene(pokemon);
 		primaryStage.show();
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@Override
+	
 	public void stop() throws Exception {
 		super.stop();
 		intro.stop();
